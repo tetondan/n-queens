a585b4d32f54611fbf406c9378754c5f4e3e845c
tetondan <dfrehner1@gmail.com>
2015-12-02 16:24:40 -0800

    passes "finds a valid solution for n of 1-8" test

--- src/solvers.js
+++ src/solvers.js
@@ -16,10 +16,14 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n: n});
+  for(var i = 0; i < n; i++){
+    solution.togglePiece(i,i);
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  console.log(solution.hasAnyRooksConflicts());
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
